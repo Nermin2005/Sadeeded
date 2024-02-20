96ed27c5cc2b72498a4ed3c70e0df916cc6390d6
iamnermin <memmedlinermin205@gmail.com>
2024-02-20 17:45:49 +0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        int eded = scanner.nextInt();
+        boolean sadedir = true;
+        for (int i = 2; i < eded; i++) {
+            if (eded % i == 0) {
+                sadedir = false;
+            }
+        }
+        if (eded==1) {
+            System.out.println("Sade eded deyil");
+            return;
+        }
+        if(eded<1) {
+            System.out.println("Duzgun reqem deyil");
+        }
+        if (sadedir == true) {
+            System.out.println("Sadedir "+eded);
+        }
+        else {
+            System.out.println("Sade deyil");
         }
     }
 }
